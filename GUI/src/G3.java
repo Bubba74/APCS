5f412c28c2c50cd2cee761bd8367b4459a341528
Henry Loh <pgloh.henry@gmail.com>
2017-03-30 10:04:30 -0700

    Added command line options to G3

--- GUI/src/G3.java
+++ GUI/src/G3.java
@@ -7,12 +7,14 @@ public class G3 {
 	public static void main (String[] args){
 		JFrame frame = new JFrame("Bulls-Eye");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(800,300,800,500);
+		frame.setBounds(800,300,800,550);
 		frame.setBackground(Color.lightGray);
+		frame.setLayout(null);
 		
 		Target target = new Target(150+250, 250, 250);
-		frame.add(target);
+		if (args.length > 0) target.setScope (Integer.parseInt(args[0]));
 
+		frame.add(target);
 		frame.setVisible(true);
 	}
 }
--- GUI/src/Target.java
+++ GUI/src/Target.java
@@ -1,27 +1,43 @@
 
 import javax.swing.JComponent;
 import java.awt.Graphics;
+import java.awt.Color;
 
 public class Target extends JComponent {
 	
 	int r;
+	int len;
 
 	public Target (int x, int y, int radius){
 		super();
 		r = radius;
 
 		super.setBounds(x-r,y-r,2*r,2*r);
+		len = 100;
+	}
+
+	public void setScope (int length){
+		len = length;
 	}
 
 	public void paint (Graphics g){
-		int len = 100;
 
-		for (int i=len; i>0; i--){
+		for (int i=len; i>len/10; i--){
 			int drawX = r -  r * i/len;
 			int drawY = r -  r * i/len;
 			int drawR = r*i/len;
 
+			if (i%2 == 0) g.setColor(Color.blue);
+			else g.setColor(Color.white);
+
 			g.fillOval(drawX, drawY, 2*drawR, 2*drawR);
 		}
+
+		int drawX = r - r/10;
+		int drawY = r - r/10;
+		int drawR = r/10;
+		
+		g.setColor(Color.red);
+		g.fillOval(drawX, drawY, 2*drawR, 2*drawR);
 	}//paint
 }
